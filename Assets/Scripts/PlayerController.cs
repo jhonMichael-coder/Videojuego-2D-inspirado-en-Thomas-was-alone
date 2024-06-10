c3725bbbe2fac7de129c7e905bee35c3bfd3ba5c
jhonMichael-coder <jmsanchez037@soy.sena.edu.co>
2024-06-10 01:10:21 -0500

    Script del jugador
    Creación de script para el control del personaje. Se incluye los componentes requeridos para el personaje: SpriteRenderer, Rigidbodu2D, BoxCollider2D, Animator.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer), typeof(Rigidbody2D), typeof(BoxCollider2D))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+
+}
